a46b920952871cf0f2e98ae360ec9b99366c3407
Mpergolatti <Mpergolatti@gmail.com>
2022-08-16 17:51:21 -0400

    Finished loop to create password

--- Develop/script.js
+++ Develop/script.js
@@ -11,7 +11,7 @@ var confirmLower = false;
 var confirmNum = false;
 var confirmSym = false;
 
-// Generate password function start
+// Generate password function start and character length restrictions
 var generatePassword = function() {
   var textSpace = "";
   var passCode = "";
@@ -21,12 +21,42 @@ var generatePassword = function() {
     if (passwordLength > 128 || passwordLength < 8 || !passwordLength) {
       window.alert("Your password must be between 8 and 128 characters long.");
       passwordLength = 0;
-    } else {
-      break;
     }
+    
   }
 
-}
+  // Executing while loop for character customization
+  while(!confirmLower && !confirmUpper && !confirmNum && !confirmSym) {
+    confirmLower = window.confirm("Would you like to include LOWER CASE characters?");
+    confirmUpper = window.confirm("Would you like to include UPPER CASE characters?");
+    confirmNum = window.confirm("Would you like to include NUMBERS?");
+    confirmSym = window.confirm("Would you like to include SYMBOLS?");
+    
+    if (confirmLower) {
+      textSpace += lowerCase;
+    }
+    if (confirmUpper) {
+      textSpace += upperCase;
+    }
+    if (confirmNum) {
+      textSpace += numbers;
+    }
+    if (confirmSym) {
+      textSpace += symbols;
+    }
+    if (!confirmLower && !confirmUpper && !confirmNum && !confirmSym)
+    window.alert("Your Password must contain at least one of the character types");
+  }
+
+  // This executes the randomization of the variables at the top of the application.
+
+  for (var i = 0; i < passwordLength; i++) {
+    passCode += textSpace [Math.floor(Math.random() * textSpace.length)]
+  }
+
+  return passCode;
+
+};
 
 
 // Get references to the #generate element
